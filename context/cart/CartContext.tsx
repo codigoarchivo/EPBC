eefc70cbeef07622d9ed78e6bfcff75541e31b41
Jackson Quintero <codigoarchivo@gmail.com>
2022-12-19 20:46:12 -0400

    context/cart/CartContext.tsx: agrego en el cartContext dos funciones en las interfaces y texto favorites

--- context/cart/CartContext.tsx
+++ context/cart/CartContext.tsx
@@ -1,11 +1,12 @@
 import { createContext } from 'react';
 
-import { ICartProduct, ShippingAddress } from '../../interfaces';
+import { ICartProduct, IProduct, ShippingAddress } from '../../interfaces';
 
 
 interface ContextProps {
     isLoaded: boolean;
     cart: ICartProduct[];
+    favorites: ICartProduct[];
     numberOfItems: number;
     subTotal: number;
     tax: number;
@@ -19,6 +20,8 @@ interface ContextProps {
     updateCartQuantity: (product: ICartProduct) => void;
     removeCartProduct: (product: ICartProduct) => void;
     updateAddress: (product: ShippingAddress) => void;
+    addFavorite: (product: ICartProduct) => void;
+    deleteFavorite: (product: ICartProduct) => void;
 
     // orders
     createOrder: () => Promise<{ hasError: boolean; message?: string }>
