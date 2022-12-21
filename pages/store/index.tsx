dce67d866fe591a43930649c72142eedeef8b356
Jackson Quintero <codigoarchivo@gmail.com>
2022-12-21 17:40:42 -0400

    pages/store/index.tsx: agrego sort random

--- pages/store/index.tsx
+++ pages/store/index.tsx
@@ -1,33 +1,34 @@
-import type { NextPage } from 'next'
-import Typography from '@mui/material/Typography'
+import type { NextPage } from 'next';
+import Typography from '@mui/material/Typography';
 import { useProducts } from '../../hooks';
 import { ShopLayout } from '../../components/layouts';
 import { ProductList } from '../../components/products';
 import { FullScreenLoading } from '../../components/ui';
 
-
-// interface Props {
-//   products: IProduct[]
-// }
-
-
 const StorePage: NextPage = () => {
-
-
   const { products, isLoading } = useProducts('/products');
-
   return (
-    <ShopLayout title={'epbc-Shop - Home'} pageDescription={'Encuentra los mejores productos de epbc aqui'}>
-      <Typography variant='h1' component='h1'>Tienda</Typography>
-      <Typography variant='h2' sx={{ mb: 1 }}>Todos los productos</Typography>
+    <ShopLayout
+      title={'epbc-Shop - Home'}
+      pageDescription={'Encuentra los mejores productos de epbc aqui'}
+    >
+      <Typography
+        variant='h1'
+        component='h1'
+      >Tienda</Typography>
+      <Typography
+        variant='h2'
+        sx={{ mb: 1 }}
+      >Todos los productos</Typography>
       {
         isLoading
           ? <FullScreenLoading />
-          : <ProductList products={products} />
+          : <ProductList
+            // sort random by products 
+            products={products.sort(() => 0.5 - Math.random())}
+          />
       }
     </ShopLayout>
   )
 }
-
-
-export default StorePage
+export default StorePage;
